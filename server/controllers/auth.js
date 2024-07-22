6d4d1073eb660a97edb428cffcce70c26f145e5c
Paras Bhandari <paras.bhandari7010@gmail.com>
2024-07-22 14:38:48 +0530

    reset password!
    added get email link to reset password functionality! (backend)

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { sendMail } from "../services/emailDetail.js";
 
 /** REGISTER USER */
 export const registerUser = async (req, res) => {
@@ -24,19 +25,90 @@ export const registerUser = async (req, res) => {
 /** LOGIN */
 export const loginUser = async(req,res)=>{
     try{
-        const { email, password } = req.body;
+        const { email, password, remember } = req.body;
         const user = await User.findOne({ email : email });
         if(!user){
-            return res.status(404).json({ message: "User Not Found!!" })
+            return res.status(404).json({ error: "User Not Found!!" })
         }
         const verifyPassword = await bcrypt.compare(password,user.password);
         if(!verifyPassword){
-            return res.status(401).json({ message: "Invaild credentials!!" })
+            return res.status(401).json({ error: "Invaild credentials!!"})
         }
-        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
-        user.password = undefined;
-        res.status(200).json({ token, user });
+        
+        if(remember === true){
+          const token = jwt.sign({ id: user._id},process.env.JWT_SECRET,{
+            expiresIn: "30d"
+          })
+          user.password = undefined;
+          user.followers = undefined;
+          user.following = undefined;
+          user.resetPasswordToken = undefined;
+          res.status(200).json({ token, user });
+        }
+        else{
+          const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "10m",
+          });
+          user.password = undefined;
+          user.followers = undefined;
+          user.following = undefined;
+          user.resetPasswordToken = undefined;
+          res.status(200).json({ token, user});
+        }
     }catch(error){
         res.status(401).json({ error: error.message })
     }
-}
+};
+
+//** RESSET PASSWORD LINK */
+export const sendLink = async(req, res)=>{
+  const { email } = req.body;
+  try{
+    const user = await User.findOne({ email: email });
+    if(user){
+      const token = jwt.sign({id: user._id},process.env.JWT_SECRET,{
+        expiresIn: '15m'
+      });
+      const updatedData = await User.findByIdAndUpdate(
+        { _id: user._id}, {resetPasswordToken: token },
+        { new: true }
+      );
+      const data = await updatedData.save();
+      const resetPasswordLink = `http://localhost:3000/reset-password/${user._id}/${token}`;
+      const mailData = {
+        toEmail: email,
+        title: "Reset Password Link For Sekai!",
+        message: `This is the link to reset password for your sekai account.< ${resetPasswordLink} >This link will expire in 10minutes. Please Do not share this link with anyone!`
+      }
+      const response = await sendMail(mailData);
+      res.status(200).json({message: response});
+    }else{
+      res.status(404).json({ error: error });
+    }
+  }
+  catch(error){
+    res.status(404).json({ error: error })
+  }
+};
+
+//** CHANGE PASSWORD */
+export const changePassword = async(req, res)=>{
+  const { id, token } = req.params;
+  const { password } = req.body;
+  try{
+      const user = await User.findOne({ _id: id });
+      if(!user) return res.status(404).json({error: "User Not Found!" });
+      const verify = jwt.verify(token, process.env.JWT_SECRET);
+      if(verify){
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password,salt);
+        const newData = await User.findByIdAndUpdate({_id:id},{password:hashPassword},{new:true});
+        await newData.save();
+        res.status(200).json({message: "Password Changed Successfully."});
+      }else{
+        res.status(403).json({error: "User is not authorized!!"});
+      }
+  }catch(error){
+      res.status(400).json({error: error})
+  }
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,16 @@
 //** IMPORTS */
 import express from "express";
-import { loginUser, registerUser } from "../controllers/auth.js";
+import { changePassword, loginUser, registerUser, sendLink } from "../controllers/auth.js";
 
 //** CONFIG */
 const router = express.Router();
 
+//** LOGIN */
 router.post("/register",registerUser);
-router.post("/login",loginUser)
+//** REGISTER */
+router.post("/login",loginUser);
+//** EMAIL AND PASSWORD */
+router.post("/resetpasswordlink",sendLink);
+router.post("/changepassword/:id/:token",changePassword);
 
 export default router;
