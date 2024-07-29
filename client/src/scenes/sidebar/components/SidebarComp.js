50aedbd281d57d8e218ce5e3a228a5cbdb6769c6
Paras Bhandari <paras.bhandari7010@gmail.com>
2024-07-29 16:42:07 +0530

    notification badge
    notification badge!

--- client/src/scenes/sidebar/components/SidebarComp.js
+++ client/src/scenes/sidebar/components/SidebarComp.js
@@ -5,6 +5,7 @@ import { Button, Typography, Avatar } from "@mui/material";
 import { useTheme } from "@emotion/react";
 import { Badge } from "@mui/material";
 import MessageIcon from "@mui/icons-material/Message";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 //** REDUCERS */
 import { useSelector } from "react-redux";
 
@@ -19,23 +20,39 @@ const SidebarComp = ({ Icon, name }) => {
       sx={{
         textTransform: "none",
         color: palette.primary.dark,
-        padding: !small ?"0.5": "1rem",
+        padding: !small ? "0.5" : "1rem",
         "&:hover": { color: palette.primary.main },
       }}
     >
-      <Box display="flex" flexDirection="row" 
-      gap={isNonMobileScreens ? "2.3rem" : null} width="100%" justifyContent=  {!isNonMobileScreens ? "center" : null}>
+      <Box
+        display="flex"
+        flexDirection="row"
+        gap={isNonMobileScreens ? "2.3rem" : null}
+        width="100%"
+        justifyContent={!isNonMobileScreens ? "center" : null}
+      >
         {Icon && (
           <>
-            {Icon === MessageIcon ? (
-              <Badge badgeContent={19} color="primary">
+            {Icon === FavoriteIcon ? (
+              <Badge badgeContent={"10M"} color="primary">
                 <Icon
                   sx={{
-                    fontSize: !small ?"30px":"25px",
+                    fontSize: !small ? "30px" : "25px",
                   }}
                 />
               </Badge>
-            ) : (
+            ) : null}
+            {Icon === MessageIcon ? (
+              <Badge badgeContent={"10k"} color="primary">
+                <Icon
+                  sx={{
+                    fontSize: !small ? "30px" : "25px",
+                  }}
+                />
+              </Badge>
+            ) : null}
+
+            {Icon === MessageIcon || Icon === FavoriteIcon ? null : (
               <Icon
                 src={Icon === Avatar && user ? user.profilePicture : null}
                 sx={{
